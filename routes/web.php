ad977ac5fa74daae98ef0e1e2f6867bf4496b5f6
gucordeiro26 <gucordeiro26@gmail.com>
2024-03-27 18:07:14 -0300

    configurando o projeto laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobrenos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
